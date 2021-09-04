7363c7c3e254db8301cadea1adb7e1b057fd516e
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-09-04 23:09:57 +0530

    Use close items data to choose answers

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,31 @@
+const cleanAnswer = (answer) => answer.toLowerCase().replace('..', ' ')
+
 const getNextQuestionAndAnswers = (data, history) => {
   const totalItems = data.length;
 
-  for (let i = 0; i < 4; i++) {
-    let answersIndices = new Set()
-    while (answersIndices.size < 4) {
-      answersIndices.add(Math.floor(Math.random() * totalItems))
-    }
-    answersIndices = Array.from(answersIndices)
-    const correctAnswer = Math.floor(Math.random() * 4)
-    const questionIndex = answersIndices[correctAnswer]
+  const questionIndex = Math.floor(Math.random() * totalItems)
+  const closestItems = data[questionIndex]['closest_items'] 
 
-    const question = data[questionIndex]['example_with_blanks']
-    const answers = answersIndices.map(i => data[i]['example_answer'])
-    const meaning = data[questionIndex]['meaning']
-    return {
-      question,
-      answers,
-      correctAnswer,
-      meaning
+  let answerOptions = new Set()
+  while (answerOptions.size < 3) {
+    const answerIndex = closestItems[Math.floor(Math.random() * closestItems.length)]
+    let answerOption = data[answerIndex]['example_answer']
+    if (answerOption !== data[questionIndex]['example_answer']) {
+      answerOptions.add(answerOption)
     }
+  }
+  answerOptions = Array.from(answerOptions)
+  const correctAnswer = Math.floor(Math.random() * 4)
+  answerOptions.splice(correctAnswer, 0, data[questionIndex]['example_answer'])
+
+  const question = data[questionIndex]['example_with_blanks']
+  const answers = answerOptions.map(cleanAnswer)
+  const meaning = data[questionIndex]['meaning']
+  return {
+    question,
+    answers,
+    correctAnswer,
+    meaning
   }
 }
 
